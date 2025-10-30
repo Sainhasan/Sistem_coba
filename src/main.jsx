6c0c13429832e08802eb3149a4896d7d11f3a4ec
Sainhasan <husain.hasan2011@gmail.com>
2025-10-31 00:50:18 +0700

    Reorder import statements for consistency in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Toaster } from "react-hot-toast";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import './index.css'
-
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Regis from './pages/Regis'
 import ManageUser from './pages/ManageUser'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
